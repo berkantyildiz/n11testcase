64067d2fa382457ee6d6466e90462d7c08aafd7b
Berkant <berkant.yildiz@bilyoner.com>
2022-07-08 19:13:02 +0300

    init

---
+++ app/src/main/java/com/n11/n11testcase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.n11.n11testcase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
